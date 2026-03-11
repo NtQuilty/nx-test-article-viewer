96f80b55e500d67bcb824abfe75b298c8a325645
NtQuilty <onlyhead14@gmail.com>
2026-03-11 23:34:05 +0300

    build: setup initial architecture and project skeleton

---
+++ packages/shared/router/src/lib/router.tsx
@@ -0,0 +1,11 @@
+import styles from './router.module.css';
+
+export function Router() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Router!</h1>
+    </div>
+  );
+}
+
+export default Router;
